8dd347a44a3ed294914163c397978348f1884ce5
FredP21 <f.pelletier21@gmail.com>
2023-12-11 13:29:42 +0100

    Created components Intro and ProfileImg for Home Page

---
+++ src/components/ProfileImg.jsx
@@ -0,0 +1,13 @@
+function ProfileImg() {
+  return (
+    <div className="fixed">
+      <img
+        src="src\assets\me.jpg"
+        alt="picture of me"
+        className="h-screen object-cover"
+      />
+    </div>
+  );
+}
+
+export default ProfileImg;
